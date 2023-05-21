4815908a1d4cf21f7e2a727360e6ab5f0e2fb799
1farag <ahmedfarag.acc1@gmail.com>
2023-05-21 07:50:48 +0300

    add signIn by email controller

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../middleware/errorHandling.js";
-import { registerServiece } from "../services/auth.services.js";
+import { registerServiece, signInServiece } from "../services/auth.services.js";
 import { CreatedResponse } from "../utils/response/response.index.js";
 
 export const register = asyncHandler(async (req, res) => {
@@ -8,4 +8,9 @@ export const register = asyncHandler(async (req, res) => {
 	res.status(response.statusCode).send(response.getResponseJSON());
 });
 
+export const signInByEmail = asyncHandler(async (req, res) => {
+	const result = await signInServiece(req);
+	const response = new CreatedResponse([result], req);
+	res.status(response.statusCode).send(response.getResponseJSON());
+});
 
